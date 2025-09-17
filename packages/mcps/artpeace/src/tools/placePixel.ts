db5a6d639ce1331c509f06ed2c012a412bb427af
alvinouille <alvinalesaint@protonmail.com>
2025-09-17 11:24:53 +0200

    feat: move tsconfig extension in packages/ to root and update all dependances

--- packages/mcps/artpeace/src/tools/placePixel.ts
+++ packages/mcps/artpeace/src/tools/placePixel.ts
@@ -28,7 +28,7 @@ export const placePixel = async (
       constants.TRANSACTION_VERSION.V3
     );
     const artpeaceContract = new Contract(artpeaceAbi, artpeaceAddr, provider);
-    const checker = new Checker(params[0].canvasId);
+    const checker = new Checker(params[0].canvasId ?? 0);
     const id = await checker.checkWorld();
     await checker.getColors();
 
